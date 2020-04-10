53f1747393014acbe80ba634d3eb9603af1f4cf0
gautambitious <mvgt.gautam@gmail.com>
2020-04-11 03:00:29 +0530

    'app'

--- num_rec/app.py
+++ num_rec/app.py
@@ -17,11 +17,16 @@ def paintapp():
         print(data)
         image_data = re.sub('^data:image/.+;base64,', '', data['img'])
         im = Image.open(BytesIO(base64.b64decode(image_data))).resize(size=(28, 28))
-        a = np.mean((np.array(im, dtype=np.uint8) * 255), axis=2)
+        a = np.mean((np.array(im, dtype=np.uint8) * 255), axis=2)*4
         model = load_model('model.h5')
         res = int(model.predict(np.array([a])).argmax())
-        return render_template('result.htm', res=str(res))
+        print('res=', res)
+        return okay(res)
     return render_template("paint.html")
+
+
+def okay(res):
+    return render_template('result.htm', res=str(res))
 
 
 if __name__ == '__main__':
