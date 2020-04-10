ab7c0525050c1848b8ec0d35852c5fa0f4728583
gautambitious <mvgt.gautam@gmail.com>
2020-04-11 02:20:31 +0530

    Flask App

--- num_rec/app.py
+++ num_rec/app.py
@@ -20,9 +20,6 @@ def paintapp():
         a = np.mean((np.array(im, dtype=np.uint8) * 255), axis=2)
         model = load_model('model.h5')
         res = int(model.predict(np.array([a])).argmax())
-        # conn = psycopg2.connect(database="paintmyown", user = "nidhin") cur = conn.cursor() cur.execute("INSERT
-        # INTO files (name, data, canvas_image) VALUES (%s, %s, %s)", [filename, data, canvas_image]) conn.commit()
-        # conn.close()
         return render_template('result.htm', res=str(res))
     return render_template("paint.html")
 
